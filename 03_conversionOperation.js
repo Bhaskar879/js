da08202eed2272662a521e9faca1530ef5c551af
Bhaskar Ruidas <122375092+Bhaskar879@users.noreply.github.com>
2024-07-05 18:45:29 +0530

    done

---
+++ 03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score = "bhaskar"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "bhaskar"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false
+// "" => false;  "Bhaskar" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************** Operations **************************
+
+let value = 33
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Bhaskar"
+let str2 = " Ruidas"
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+// console.log(+true); //1
+// console.log(true-); //error
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
